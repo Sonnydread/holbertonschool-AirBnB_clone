231898364dcb1a98b2567b51ee1b8ada9bd551e7
Sonnydread <alvaradoguille590@gmail.com>
2023-07-07 12:34:03 -0700

    modles

--- models/base_model.py
+++ models/base_model.py
@@ -2,31 +2,29 @@
 """BaseModel class"""
 
 
-import models
-import datetime
+import json
 import uuid
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """BaseModel attributes n methods"""
     def __init__(self, *args, **kwargs):
         """BaseModel attributes n methods"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-        else:
-            for keys, value in kwargs.items():
-                if keys == "__class__":
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k == '__class__':
                     continue
-                if keys == 'id':
-                    self.id = value
-                elif keys == 'created_at':
-                    self.created_at = datetime.datetime.fromisoformat(value)
-                elif keys == 'updated_at':
-                    self.updated_at = datetime.datetime.fromisoformat(value)
-                else:
-                    setattr(self, keys, value)
+                elif k == 'updated_at' or k == 'created_at':
+                    v = datetime.fromisoformat(v)
+                setattr(self, k, v)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """BaseModel attributes n methods"""
@@ -34,11 +32,13 @@ class BaseModel:
 
     def save(self):
         """BaseModel attributes n methods"""
-        self.updated_at = datetime.datetime.now()
-
+        self.updated_at = datetime.now()
+        storage.save()
+        
     def to_dict(self):
         """BaseModel attributes n methods"""
-        return dict(self.__dict__, **{'__class__':
-                    self.__class__.__name__, 'created_at':
-                    self.created_at.isoformat(), 'updated_at':
-                    self.updated_at.isoformat()})
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = new_dict['created_at'].isoformat()
+        new_dict['updated_at'] = new_dict['updated_at'].isoformat()
+        return new_dict
