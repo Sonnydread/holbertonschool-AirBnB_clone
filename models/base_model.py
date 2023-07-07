82ab94d0555eb128c4704b204705b513b52a0a7a
Sonnydread <alvaradoguille590@gmail.com>
2023-07-07 07:07:57 -0700

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""BaseModel class"""
+
+
+import models
+import datetime
+import uuid
+
+
+class BaseModel:
+    """BaseModel attributes n methods"""
+    def __init__(self, *args, **kwargs):
+        """BaseModel attributes n methods"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        else:
+            for keys, value in kwargs.items():
+                if keys == "__class__":
+                    continue
+                if keys == 'id':
+                    self.id = value
+                elif keys == 'created_at':
+                    self.created_at = datetime.datetime.fromisoformat(value)
+                elif keys == 'updated_at':
+                    self.updated_at = datetime.datetime.fromisoformat(value)
+                else:
+                    setattr(self, keys, value)
+
+    def __str__(self):
+        """BaseModel attributes n methods"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})"
+
+    def save(self):
+        """BaseModel attributes n methods"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """BaseModel attributes n methods"""
+        return dict(self.__dict__, **{'__class__':
+                    self.__class__.__name__, 'created_at':
+                    self.created_at.isoformat(), 'updated_at':
+                    self.updated_at.isoformat()})
